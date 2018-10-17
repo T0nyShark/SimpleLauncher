f4545665e6345def4b059ef736dd0f9441ed9365
T0nyShark <bencpb@gmail.com>
2018-10-18 02:16:47 +0300

    Initial commit

---
+++ app/src/main/java/com/industries/shark/simplelauncher/SimpleLauncherActivity.java
@@ -0,0 +1,14 @@
+package com.industries.shark.simplelauncher;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleLauncherActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return SimpleLauncherFragment.newInstance();
+    }
+}
